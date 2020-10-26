10768f34886048cd6a68c2a12cd69c6c26029bc1
internetcharles <charlesodellsmith@gmail.com>
2020-10-26 13:48:19 -0700

    list page

--- src/components/list/ListPage.jsx
+++ src/components/list/ListPage.jsx
@@ -1,2 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Character from './Character';
+
+const ListPage = ({ characters }) => {
+  const characterElements = characters.map(character => (
+    <li key={character.name}>
+      <Character {...character} />
+    </li>
+  ));
+
+  return (
+    <ul data-testid="list">
+      {characterElements}
+    </ul>
+  );
+};
+
+ListPage.propTypes = {
+  characters: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    occupation: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired
+  }))
+};
+
+export default ListPage;
--- src/components/list/ListPage.test.jsx
+++ src/components/list/ListPage.test.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render, cleanup } from '@test-library/react';
+import { render } from '@testing-library/react';
 import ListPage from './ListPage';
 
 describe('ListPage', () => {
   it('renders ListPage', () => {
-    
-  })
-})
+    const { asFragment } = render(<ListPage
+      characters={[
+        { 
+          name: "John",
+          occupation: "Guy",
+          status: "Alive"
+        }
+      ]}  
+    />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
